75a92dd0ff98b5d6a6a7e3dd6ae8e8a130de6a73
cmrib <ciceromirandar@gmail.com>
2022-08-04 10:23:20 -0300

    add counter info

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
-import { Container, InputContainer, AddButton, Icon } from "./styles";
+import { Container, InputContainer, AddButton, Icon, Info, InfoContainer, Created, Done, Counter, Background } from "./styles";
 import { useTheme } from "styled-components";
+import React from "react";
+import { View } from "react-native";
 
 export function Home() {
 
@@ -20,6 +22,31 @@ export function Home() {
                     <Icon name="plus-circle" />
                 </AddButton>
             </InputContainer>
+
+            <Info>
+                <InfoContainer>
+                    <Created>
+                        Criadas
+                    </Created>
+                    <Background >
+                        <Counter>
+                            0
+                        </Counter>
+                    </Background>
+
+                </InfoContainer>
+
+                <InfoContainer>
+                    <Done>
+                        Concluídas
+                    </Done>
+                    <Background>
+                        <Counter>
+                            0
+                        </Counter>
+                    </Background>
+                </InfoContainer>
+            </Info>
 
         </Container>
     )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { Feather } from '@expo/vector-icons'
+import { RFValue } from 'react-native-responsive-fontsize';
 
 
 export const Container = styled.View`
@@ -31,4 +32,41 @@ export const Icon = styled(Feather)`
     font-size: 22px;
     color: ${({ theme }) => theme.colors.gray_100};
      
+`;
+
+export const Info = styled.View`
+    margin-top: ${RFValue(32)}px;
+    flex-direction: row;
+    padding: 0 24px;
+    justify-content: space-between;   
+`;
+
+export const Created = styled.Text`
+    color: ${({ theme }) => theme.colors.blue};
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(14)}px;
+`;
+
+export const Done = styled.Text`
+    color: ${({ theme }) => theme.colors.purple};
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(14)}px;
+`;
+
+export const InfoContainer = styled.View`
+    flex-direction: row;
+    align-items: center;  
+`;
+
+export const Background = styled.View`
+    margin-left: 8px;
+    padding: 2px 8px;
+    background-color: ${({ theme }) => theme.colors.gray_400};
+    border-radius: 50%;
+`;
+
+export const Counter = styled.Text`
+    color: ${({ theme }) => theme.colors.gray_200};
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(14)}px;
 `;
